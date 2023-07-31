20f5b749dafb344e0eae012ae1db89a03dca23ba
João Batista <joao42lbatista@gmail.com>
2023-07-31 19:49:04 -0300

    Update opera_builder.sh

--- opera_builder.sh
+++ opera_builder.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Based on: https://github.com/ivan-hc/Opera-appimage
 #
+set -x
 
 # DOWNLOAD EXTRA-LIBS
 mkdir -p lib_extra/tmp/
@@ -26,7 +27,6 @@ chmod a+x *.so
 cd ../
 
 # CREATE OPERA STABLE APPIMAGE
-APP=opera
 mkdir tmp/
 cp -r lib_extra/ tmp/
 cd tmp/
@@ -34,30 +34,33 @@ wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appima
 wget https://github.com/AppImage/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage
 chmod +x appimagetool-x86_64.AppImage
 
-DEB=$(wget -q https://deb.opera.com/opera-stable/pool/non-free/o/opera-stable/ -O - | grep deb | tail -1 | grep -o -P '(?<=.deb">).*(?=</a>)')
-wget https://deb.opera.com/opera-stable/pool/non-free/o/opera-stable/"$DEB"
+version_deb=$(wget -q https://deb.opera.com/opera-stable/pool/non-free/o/opera-stable/ -O - | grep deb | tail -1 | grep -o -P '(?<=.deb">).*(?=</a>)')
+wget "https://deb.opera.com/opera-stable/pool/non-free/o/opera-stable/$version_deb"
 ar x *.deb
 tar xf data.tar.xz
-mkdir $APP.AppDir
-mv usr/lib/x86_64-linux-gnu/opera/* $APP.AppDir/
-mv usr/share/applications/*.desktop $APP.AppDir/
-sed -i -e '/TargetEnvironment/d' $APP.AppDir/*.desktop
-mv usr/share/pixmaps/* $APP.AppDir/
+mkdir opera.AppDir
+mv usr/lib/x86_64-linux-gnu/opera/* opera.AppDir/
+mv usr/share/applications/*.desktop opera.AppDir/
+sed -i -e '/TargetEnvironment/d' opera.AppDir/*.desktop
+mv usr/share/pixmaps/* opera.AppDir/
 tar xf control.tar.xz
 VERSION=$(cat control | grep -i version | cut -c 10-)
 
-cat >> $APP.AppDir/AppRun << 'EOF'
+cat >> opera.AppDir/AppRun << 'EOF'
 #!/bin/sh
 APP=opera
 HERE="$(dirname "$(readlink -f "${0}")")"
 export UNION_PRELOAD="${HERE}"
 export LD_LIBRARY_PATH=/lib/:/lib64/:/lib/x86_64-linux-gnu/:/usr/lib/:"${HERE}"/lib_extra/:LD_LIBRARY_PATH
-exec "${HERE}"/$APP "$@"
+exec "${HERE}"/opera "$@"
 EOF
-chmod +x $APP.AppDir/AppRun
+chmod +x opera.AppDir/AppRun
 
-mv lib_extra $APP.AppDir/
+mv lib_extra opera.AppDir/
 
-ARCH=x86_64 ./appimagetool-x86_64.AppImage -n $APP.AppDir/
+ARCH=x86_64 ./appimagetool-x86_64.AppImage -n opera.AppDir/
+
+pwd
+ls -lah
 cd ..
 mv tmp/*AppImage Opera-"$VERSION"-x86_64.AppImage
